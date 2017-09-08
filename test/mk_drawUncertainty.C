9f2eaa68fbd542bce3eda43d38be06c21bd43da1
Mikko Voutilainen <mikko.voutilainen@cern.ch>
2017-09-08 19:29:23 +0300

    Add test files for drawing JEC uncertainty.

---
+++ test/mk_drawUncertainty.C
@@ -0,0 +1,15 @@
+{
+  // JEC central value
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/Utilities.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/JetCorrectorParameters.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/SimpleJetCorrector.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc+");
+  // JEC uncertainties
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/SimpleJetCorrectionUncertainty.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/JetCorrectionUncertainty.cc+");
+
+  // drawing macro
+  gROOT->ProcessLine(".L drawUncertainty.C+g");
+
+  drawUncertainty("TotalNoFlavorNoTime","AK5PFchs");
+}
