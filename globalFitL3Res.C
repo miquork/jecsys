d6ed94279ca9a550d8e7f1f68a371feba7c52ea1
Henning Kirschenmann <henning.kirschenmann@cern.ch>
2018-02-23 17:04:13 +0200

    include possibility for batch-reprocessing individidual run periods

--- globalFitL3Res.C
+++ globalFitL3Res.C
@@ -271,7 +271,7 @@ void globalFitL3Res(double etamin = 0, double etamax = 1.3,
   const int izmm = -1;
   */
 
-
+  
   // Global fit without multijets/dijets
   const int nsamples = 3;
   const int nsample0 = 0; // first Z/gamma+jet sample
@@ -279,8 +279,8 @@ void globalFitL3Res(double etamin = 0, double etamax = 1.3,
   const int igj = 0;
   const int izee = 1;
   const int izmm = 2;
-
   
+
 
   /*
   // Global fit without photon+jet
