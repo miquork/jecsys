7c0a8dd3b82b1ace9fac9c05241244cb5b0da23b
Mikko Voutilainen <voutila@Mikkos-MacBook.local>
2019-03-04 21:18:22 +0200

    Update minitools/runAllIOVs.py for 2018.

--- minitools/runAllIOVs.py
+++ minitools/runAllIOVs.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 import os
 
-IOV_list= ['BCDEFGH','BCD','EF','GH']
+IOV_list= ['A','B','C','ABC']
 for iov in IOV_list:
     os.system("mkdir pdf/"+iov)
     os.system("root -b -q 'mk_reprocess_RunEpoch.C(\""+iov+"\")'")
