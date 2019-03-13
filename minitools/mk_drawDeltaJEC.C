037f928c680c0098ab5bfe6405b08e66bfb2a0de
Mikko Voutilainen <mikko.voutilainen@cern.ch>
2019-03-13 16:51:34 +0200

    Add minitools/drawDeltaJEC.C to check JEC stability with inclusive jets.

---
+++ minitools/mk_drawDeltaJEC.C
@@ -0,0 +1,32 @@
+{
+
+  // For JEC (for uncertainty)
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/Utilities.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/JetCorrectorParameters.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/SimpleJetCorrector.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc+");
+
+  // For JEC uncertainty
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/SimpleJetCorrectionUncertainty.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/JetCorrectionUncertainty.cc+");
+
+  gROOT->ProcessLine(".L minitools/drawDeltaJEC.C+g");
+
+  //unfold("0.0-0.5",1);
+  //unfold("0.5-1.0",2);
+  //unfold("1.0-1.5",3);
+  //unfold("1.5-2.0",4);
+  //unfold("2.0-2.5",5);
+  //unfold("2.5-3.0",6);
+  //unfold("3.2-4.7",7);
+
+
+  drawDeltaJEC("0.0-0.5");
+  drawDeltaJEC("0.5-1.0");
+  drawDeltaJEC("1.0-1.5");
+  drawDeltaJEC("1.5-2.0");
+  drawDeltaJEC("2.0-2.5");
+  drawDeltaJEC("2.5-3.0");
+  drawDeltaJEC("3.2-4.7");
+
+}
